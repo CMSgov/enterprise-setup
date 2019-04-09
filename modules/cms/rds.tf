af19f87370f1a3ce4bd97837a8ae7d5f4caa650b
Mike Smorul <mike@navahq.com>
2019-04-09 10:16:33 -0400

    skeleton for rds

---
+++ modules/cms/rds.tf
@@ -0,0 +1,83 @@
+#
+# Externalize RDS.
+#
+#
+# Define postgres ingress. This only allows access in from the ecs tasks
+#
+resource "aws_security_group" "postgres_ingress" {
+  name        = "${var.application}-${var.stack}-postgres-ingress"
+  description = "allow inbound access from ECS tasks only"
+  vpc_id      = "${module.network.vpc_id}"
+  tags        = "${module.tags.application_tags}"
+
+  ingress {
+    protocol        = "tcp"
+    from_port       = "5432"
+    to_port         = "5432"
+    security_groups = ["${module.testapp-service.ecs_security_group_ids}"]
+  }
+}
+
+module "rds_postgres" {
+  source = "terraform-aws-modules/rds/aws"
+
+  name              = "${data.aws_ssm_parameter.rds_app_db.value}"
+  identifier        = "${var.application}-${var.stack}-db"
+  engine            = "postgres"
+  engine_version    = "10.6"
+  instance_class    = "${var.rds_instance}"
+  allocated_storage = 5
+  storage_encrypted = true
+
+  # uncomment to use specific/non-default key
+  # kms_key_id        = "arm:aws:kms:<region>:<account id>:key/<kms key id>"
+
+  # NOTE: Do NOT use 'user' as the value for 'username' as it throws:
+  # "Error creating DB Instance: InvalidParameterValue: MasterUsername
+  # user cannot be used as it is a reserved word used by the engine"
+  username = "${data.aws_ssm_parameter.rds_admin_user.value}"
+  password               = "${data.aws_ssm_parameter.rds_admin_pass.value}"
+  port                   = "5432"
+  vpc_security_group_ids = ["${aws_security_group.postgres_ingress.id}"]
+  maintenance_window     = "Mon:00:00-Mon:03:00"
+  backup_window          = "03:00-06:00"
+  # disable backups to create DB faster
+  backup_retention_period = 0
+  tags                    = "${module.tags.application_tags}"
+  # DB subnet group
+  subnet_ids = ["${module.network.private_subnet_ids}"]
+  # DB parameter group
+  family = "postgres10"
+  # DB option group
+  major_engine_version = "10.6"
+  # Snapshot name upon DB deletion
+  # final_snapshot_identifier = "circleci"
+  # Database Deletion Protection
+  deletion_protection = true
+  # Enable enhanced monitoring
+  # For this demo, we will create role automatically.
+  create_monitoring_role = true
+  monitoring_role_name = "${var.application}-${var.stack}-rds-monitoring"
+  monitoring_interval  = "30"
+}
+
+#
+# Store the RDS hostname in 'rds_db_host'
+#
+resource "aws_ssm_parameter" "rds_db_host" {
+  name      = "${local.ssm_prefix}rds_db_host"
+  type      = "String"
+  overwrite = true
+  value     = "${module.rds_postgres.this_db_instance_address}"
+}
+
+#
+# SSM Parameters used by this application
+#
+data "aws_ssm_parameter" "rds_admin_user" {
+  name = "${local.ssm_prefix}rds_admin_user"
+}
+
+data "aws_ssm_parameter" "rds_admin_pass" {
+  name = "${local.ssm_prefix}rds_admin_pass"
+}
